1048a0d53ee10d05ab74d4d527160323fe300220
gtorresv <giselle.torres15@gmail.com>
2023-10-22 20:54:16 -0400

    added navbar component and pages

--- react-portfolio/src/App.jsx
+++ react-portfolio/src/App.jsx
@@ -1,34 +1,28 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Home from './pages/Home'
+import { Navbar } from './components/Navbar'
+import Portfolio from './pages/Portfolio';
+import Contact from './pages/Contact';
+import Resume from './pages/Resume';
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [currentPage, setCurrentpage] = useState('home');
+  const onPageChange = () => {
+    if (currentPage === 'home') {
+      return <Home />
+    } else if (currentPage === 'portfolio') {
+      return <Portfolio />
+    } else if (currentPage === 'contact') {
+      return <Contact />
+    } else if (currentPage === 'resume') {
+      return <Resume />
+    }
+  }
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <Navbar setCurrentpage={setCurrentpage}/>
+      {onPageChange()}
+    </div>
   )
 }
 
