cb59e8a3a786383ddf8929864fc14182f718808f
gtorresv <giselle.torres15@gmail.com>
2023-10-27 20:05:11 -0400

    edited the contact form and added some styling

--- react-portfolio/src/pages/Contact.jsx
+++ react-portfolio/src/pages/Contact.jsx
@@ -1,19 +1,19 @@
 
 const Contact = () => {
   return (
-    <div>
+    <div className="contact-section">
       <h2>Contact</h2>
       <div className="form">
       <div className="mb-3">
-        <label htmlFor="exampleFormControlInput1" className="form-label">Email address</label>
+        <label htmlFor="exampleFormControlInput1" className="form-label">Email address:</label>
         <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com" />
       </div>
       <div className="mb-3">
-        <label htmlFor="exampleFormControlInput1" className="form-label">Name</label>
-        <input type="name" className="form-control" id="exampleFormControlInput2" placeholder="enter name" />
+        <label htmlFor="exampleFormControlInput1" className="form-label">Name:</label>
+        <input type="name" className="form-control" id="exampleFormControlInput2" />
       </div>
       <div className="mb-3">
-        <label htmlFor="exampleFormControlTextarea1" className="form-label">Example textarea</label>
+        <label htmlFor="exampleFormControlTextarea1" className="form-label">Message:</label>
         <textarea className="form-control" id="exampleFormControlTextarea1" rows={3} defaultValue={""} />
       </div>
     </div>
